82408f4904b6af975a0f5c8f22e0d87fb76cbebc
Devanshvashishta <devanshvashishta@gmail.com>
2021-07-25 16:06:57 +0530

    netflix clone completed on 25th July

--- src/Row.js
+++ src/Row.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { useState,useEffect } from 'react';
 import axios from './axios';
 import './Row.css';
-
+import YouTube from 'react-youtube';
+import movieTrailer from 'movie-trailer';
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
 function Row({title, fetchUrl,isLargeRow}) {
 const [movies, setMovies] = useState([]);
+const [trailerUrl,setTrailerUrl] = useState("");
 
 
 useEffect(() =>{
@@ -19,7 +21,30 @@ return request;
 fetchData();
 },[fetchUrl]);
 
-console.table(movies);    
+const opts = {
+    height: '390',
+    width: '100%',
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+    },
+  };
+
+  const handleClick = (movie) =>{
+      if (trailerUrl){
+          setTrailerUrl('');
+      }
+      else {
+          movieTrailer(movie?.name || "")
+          .then( (url) => { 
+              const urlParams = new URLSearchParams(new URL(url).search)  ;
+              setTrailerUrl(urlParams.get('v'));
+
+          }).catch((error) => console.log(error));
+      }
+
+  };
+    
 
     return (
         <div className="row">
@@ -30,12 +55,13 @@ console.table(movies);
          {movies.map(movie =>(
              <img 
              key={movie.id}
+             onClick={() => handleClick(movie)}
              className={ `row_poster ${isLargeRow && "row__posterLarge"}`}
              src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
              alt={movie.name}/>
          ))}
          </div>
-        
+            {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
         </div>
     )
 
